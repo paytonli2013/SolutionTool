8ba836d05b45503b361ab240c1efc711990178b1
paytonli2013 <payton.com@163.com>
2013-12-23 14:53:25 +0800

    commit file dirtrectory rules

---
+++ src/SolutionTool/slntool/DirectoryExistedRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orc.SolutionTool
+{
+    public class DirectoryExistedRule : FileStructureRule
+    {
+
+        protected override void InnerExam(FileStructureRuleExamContext context, Action<ExamResult> onComplete)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InnerApply(FileStructureRuleExamContext fileStructureRuleExamContext, Action<ExamResult> onComplete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SolutionTool/slntool/FileStructureRuleEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orc.SolutionTool
+{
+    class FileStructureRuleEnum
+    {
+    }
+}

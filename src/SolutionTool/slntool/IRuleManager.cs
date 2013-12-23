32f79040161eb0889d72dd1a3e562b0d869ac218
paytonli2013 <payton.com@163.com>
2013-12-23 15:47:30 +0800

    commit IRuleManage

---
+++ src/SolutionTool/slntool/IRuleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orc.SolutionTool
+{
+    public interface IRuleManager
+    {
+        void Persist(IRuleSet ruleSet, Action<bool, Exception> onComplete);
+
+        void Load(string ruleSet, Action<IRuleSet, Exception> omComplete);
+    }
+}

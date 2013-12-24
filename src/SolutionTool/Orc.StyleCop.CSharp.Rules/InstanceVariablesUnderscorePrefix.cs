96cc7f4bd91da5b7b2cfd0a589f4438f1807aa41
Neo <sukoora@qq.com>
2013-12-24 15:30:19 +0800

    Adding StyleCop custom rules and CommandLineParser package.

---
+++ src/SolutionTool/Orc.StyleCop.CSharp.Rules/InstanceVariablesUnderscorePrefix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StyleCop;
+using StyleCop.CSharp;
+
+namespace Orc.StyleCop.CSharp.Rules
+{
+    /// <summary>
+    /// This StyleCop Rule makes sure that instance variables are prefixed with an underscore.
+    /// </summary>
+    [SourceAnalyzer(typeof(CsParser))]
+    public class InstanceVariablesUnderscorePrefix : SourceAnalyzer
+    {
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument csdocument = (CsDocument)document;
+            if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+                csdocument.WalkDocument(new CodeWalkerElementVisitor<object>(this.VisitElement), null, null);
+        }
+
+        private bool VisitElement(CsElement element, CsElement parentElement, object context)
+        {
+            // Flag a violation if the instance variables are not prefixed with an underscore.
+            if (!element.Generated && element.ElementType == ElementType.Field && element.ActualAccess != AccessModifierType.Public &&
+                element.ActualAccess != AccessModifierType.Internal && element.Declaration.Name.ToCharArray()[0] != '_')
+            {
+                AddViolation(element, "InstanceVariablesUnderscorePrefix");
+            }
+            return true;
+        }
+    }
+}
--- src/SolutionTool/slntool/Program.cs
+++ src/SolutionTool/slntool/Program.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace slntool
+namespace Orc.SolutionTool
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // Values are available here
+                if (options.Verbose) 
+                    Console.WriteLine("Filename: {0}", options.InputFile);
+            }
         }
     }
 }

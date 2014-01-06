989286c4e3579dc92d684d36239ed423863b31cc
Neo <sukoora@qq.com>
2014-01-06 21:10:36 +0800

    Managing templates via XML.

--- src/SolutionTool/SolutionTool.Infrasturcture/Model/TemplateManager.cs
+++ src/SolutionTool/SolutionTool.Infrasturcture/Model/TemplateManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace Orc.SolutionTool.Model
@@ -32,22 +32,17 @@ namespace Orc.SolutionTool.Model
             }
         }
 
-        public void LoadTemplate(string templateFileName, Action<Repository, Exception> onComplete)
+        public void LoadTemplate(string templateFileName, Action<string, Exception> onComplete)
         {
             var exception = null as Exception;
-            var repository = null as Repository;
+            var directory = null as string;
             var path = System.IO.Path.Combine(_dir, templateFileName);
 
             if (System.IO.File.Exists(path))
             {
                 try
                 {
-                    using (var fs = System.IO.File.OpenRead(path))
-                    {
-                        var xs = new XmlSerializer(typeof(Repository));
-
-                        repository = xs.Deserialize(fs) as Repository;
-                    }
+                    directory = System.IO.File.ReadAllText(path);
                 }
                 catch (Exception xe)
                 {
@@ -57,11 +52,11 @@ namespace Orc.SolutionTool.Model
 
             if (onComplete != null)
             {
-                onComplete(repository, exception);
+                onComplete(directory, exception);
             }
         }
 
-        public void SaveTemplate(string templateFileName, Repository repository, Action<bool, Exception> onComplete)
+        public void SaveTemplate(string templateFileName, string templateXmlContent, Action<bool, Exception> onComplete)
         {
             var exception = null as Exception;
             var isOk = true;
@@ -72,13 +67,75 @@ namespace Orc.SolutionTool.Model
                 System.IO.File.Delete(path);
             }
 
+            ValidateTemplate(templateXmlContent, (x, y) => 
+            {
+                if (!x)
+                {
+                    isOk = false;
+                    exception = new Exception("Not a valid template file! ");
+                }
+
+                try
+                {
+                    System.IO.File.WriteAllText(path, templateXmlContent);
+                }
+                catch (Exception xe)
+                {
+                    isOk = false;
+                    exception = xe;
+                }
+
+                if (onComplete != null)
+                {
+                    onComplete(isOk, exception);
+                }
+
+            });
+        }
+
+        public void DeleteTemplate(string templateFileName, Action<bool, Exception> onComplete)
+        {
+            var exception = null as Exception;
+            var isOk = true;
+            var path = System.IO.Path.Combine(_dir, templateFileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                try
+                {
+                    System.IO.File.Delete(path);
+                }
+                catch (Exception xe)
+                {
+                    exception = xe;
+                    isOk = false;
+                }
+            }
+
+            if (onComplete != null)
+            {
+                onComplete(isOk, exception);
+            }
+        }
+
+
+        public void ValidateTemplate(string templateXmlContent, Action<bool, Exception> onComplete)
+        {
+            var exception = null as Exception;
+            var isValid = false;
+
             try
             {
-                using (var sw = System.IO.File.CreateText(path))
+                using (var sr = new System.IO.StringReader(templateXmlContent))
                 {
-                    var xs = new XmlSerializer(typeof(Repository));
+                    var xs = new XmlSerializer(typeof(Directory));
 
-                    xs.Serialize(sw, repository);
+                    var directory = xs.Deserialize(sr) as Directory;
+
+                    if (directory != null)
+                    {
+                        isValid = true;
+                    }
                 }
             }
             catch (Exception xe)
@@ -88,7 +145,7 @@ namespace Orc.SolutionTool.Model
 
             if (onComplete != null)
             {
-                onComplete(isOk, exception);
+                onComplete(isValid, exception);
             }
         }
     }

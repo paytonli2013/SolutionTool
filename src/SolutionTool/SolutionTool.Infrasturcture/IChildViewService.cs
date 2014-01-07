ff2f788c112ba40767c09d530e4b2557fd4e1b14
paytonli2013 <payton.com@163.com>
2014-01-07 18:05:10 +0800

    IChildViewService

---
+++ src/SolutionTool/SolutionTool.Infrasturcture/IChildViewService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orc.SolutionTool
+{
+    public interface IChildViewService
+    {
+        void OpenChildView(IWindowHost host, string viewName, string title, Action<CloseResult> onClosed, ViewOptions option = null);
+    }
+
+    public class ViewOptions
+    {
+        public double Height { get; set; }
+
+        public double Width { get; set; }
+
+        public static ViewOptions Default = new ViewOptions() { Height = 800, Width = 600 };
+    }
+}
